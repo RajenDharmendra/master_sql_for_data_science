9f5c3b4737966a71584f0d381dbd05e1f1f4947f
Charlie O'Hara <charlie.b.ohara@gmail.com>
2019-05-06 20:55:53 -0700

    complete lectures

--- setup.sql
+++ setup.sql
@@ -1087,3 +1087,21 @@ insert into fruit_imports values(12, 'Apricot', 'Summer', 'Kansas', 14500, 0.20)
 insert into fruit_imports values(13, 'Mango', 'Summer', 'Texas', 17000, 0.68);
 insert into fruit_imports values(14, 'Pear', 'Fall', 'Nebraska', 30500, 0.12);
 insert into fruit_imports values(15, 'Grape', 'Fall', 'Illinois', 72500, 0.35);
+
+CREATE TABLE sales
+(
+	continent varchar(20),
+	country varchar(20),
+	city varchar(20),
+	units_sold integer
+);
+
+INSERT INTO sales VALUES ('North America', 'Canada', 'Toronto', 10000);
+INSERT INTO sales VALUES ('North America', 'Canada', 'Montreal', 5000);
+INSERT INTO sales VALUES ('North America', 'Canada', 'Vancouver', 15000);
+INSERT INTO sales VALUES ('Asia', 'China', 'Hong Kong', 7000);
+INSERT INTO sales VALUES ('Asia', 'China', 'Shanghai', 3000);
+INSERT INTO sales VALUES ('Asia', 'Japan', 'Tokyo', 5000);
+INSERT INTO sales VALUES ('Europe', 'UK', 'London', 6000);
+INSERT INTO sales VALUES ('Europe', 'UK', 'Manchester', 12000);
+INSERT INTO sales VALUES ('Europe', 'France', 'Paris', 5000);
