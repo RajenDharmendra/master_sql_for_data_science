c79d3d23023c1959f5b02b7a0371af71e1ee20a4
Charlie O'Hara <charlie.b.ohara@gmail.com>
2019-05-06 21:18:09 -0700

    final assignment

---
+++ 8.sql
@@ -0,0 +1,10 @@
+--Write a query that finds students who do not take CS180.
+
+--Write a query to find students who take CS110 or CS107 but not both.
+
+--Write a query to find students who take CS220 and no other courses.
+
+--Write a query that finds those students who take at most 2 courses.
+--Your query should exclude students that don't take any courses as well as those  that take more than 2 course. 
+
+--Write a query to find students who are older than at most two other students.
