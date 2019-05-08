d637bd3857a22d66b26459e97f081ddd988b3704
Charlie O'Hara <charlie.b.ohara@gmail.com>
2019-05-07 20:03:58 -0700

    complete course

--- 8.sql
+++ 8.sql
@@ -1,42 +1,33 @@
 --Write a query that finds students who do not take CS180.
-select * from students where student_no in
-(select distinct student_no
+select * from students
+where student_no not in 
+(select student_no
 from student_enrollment
-where course_no != 'CS180');
+where course_no = 'CS180');
 
 --Write a query to find students who take CS110 or CS107 but not both.
-select * from students
-where student_no in 
-(select student_no 
-from student_enrollment
-where course_no = 'CS110')
-or student_no in
-(select student_no 
-from student_enrollment
-where course_no = 'CS107');
+select s.student_no, s.student_name, s.age
+from students s, student_enrollment se
+where s.student_no = se.student_no
+group by s.student_no, s.student_name, s.age
+having sum(case when se.course_no in ('CS110', 'CS107') then 1 else 0 end) = 1;
 
 --Write a query to find students who take CS220 and no other courses.
-select * from students
-where student_no in 
-(select student_no from student_enrollment
-where course_no = 'CS220')
-and student_no not in
-(select student_no 
-from student_enrollment
-where course_no != 'CS220');
+select s.*
+from students s, student_enrollment se
+where s.student_no = se.student_no
+and s.student_no not in 
+(select student_no from student_enrollment where course_no != 'CS220');
 
 --Write a query that finds those students who take at most 2 courses.
 --Your query should exclude students that don't take any courses as well as those  that take more than 2 course. 
-from students where student_no in
-(select student_no
-from student_enrollment
-group by student_no
-having count(course_no) = 2);
-select * 
-from students where student_no in
-(select student_no
-from student_enrollment
-group by student_no
-having count(course_no) <= 2);
+select s.student_no, s.student_name, s.age
+from students s, student_enrollment se
+where s.student_no = se.student_no
+group by s.student_no, s.student_name, s.age
+having count(*) <= 2;
 
 --Write a query to find students who are older than at most two other students.
+select s1.*
+from students s1
+where 2 >= (select count(*) from students s2 where s2.age < s1.age);
